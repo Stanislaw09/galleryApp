2315cd271c4a3d0ec92fce0f33a485cfe9e88e6f
Stanisław Gardzielewski <55945192+Stanislaw09@users.noreply.github.com>
2020-09-14 12:41:49 +0200

    adding build files

---
+++ precache-manifest.da31dd8e5de7d8c96408ca5f1155526e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba05625b26a4e9859da5e0dbfe8f033",
+    "url": "/galleryApp/index.html"
+  },
+  {
+    "revision": "7e6d2d96144642202f01",
+    "url": "/galleryApp/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "87f85566fde3b91a60fd",
+    "url": "/galleryApp/static/js/2.41b3c6aa.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/galleryApp/static/js/2.41b3c6aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e6d2d96144642202f01",
+    "url": "/galleryApp/static/js/main.e2ad0f3e.chunk.js"
+  },
+  {
+    "revision": "7a888c32c7c4d8d5b670",
+    "url": "/galleryApp/static/js/runtime-main.3adf1eda.js"
+  }
+]);
